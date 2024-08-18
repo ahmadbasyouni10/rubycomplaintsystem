43f09062d3ea37e6c3f4325223b4c713dd0bb7ac
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-08-18 09:01:16 -0400

    generating embeddings model level

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,70 +1,50 @@
 class ComplaintsController < ApplicationController
-  before_action :set_complaint, only: %i[ show edit update destroy ]
+  before_action :set_complaint, only: %i[show edit update destroy]
 
-  # GET /complaints or /complaints.json
   def index
     @complaints = Complaint.all
   end
 
-  # GET /complaints/1 or /complaints/1.json
   def show
   end
 
-  # GET /complaints/new
   def new
     @complaint = Complaint.new
   end
 
-  # GET /complaints/1/edit
   def edit
   end
 
-  # POST /complaints or /complaints.json
   def create
     @complaint = Complaint.new(complaint_params)
 
-    respond_to do |format|
-      if @complaint.save
-        format.html { redirect_to complaint_url(@complaint), notice: "Complaint was successfully created." }
-        format.json { render :show, status: :created, location: @complaint }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @complaint.errors, status: :unprocessable_entity }
-      end
+    if @complaint.save
+      redirect_to @complaint, notice: 'Complaint was successfully created and categorized.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /complaints/1 or /complaints/1.json
   def update
-    respond_to do |format|
-      if @complaint.update(complaint_params)
-        format.html { redirect_to complaint_url(@complaint), notice: "Complaint was successfully updated." }
-        format.json { render :show, status: :ok, location: @complaint }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @complaint.errors, status: :unprocessable_entity }
-      end
+    if @complaint.update(complaint_params)
+      redirect_to @complaint, notice: 'Complaint was successfully updated and re-categorized.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /complaints/1 or /complaints/1.json
   def destroy
-    @complaint.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to complaints_url, notice: "Complaint was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @complaint.destroy
+    redirect_to complaints_url, notice: 'Complaint was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_complaint
-      @complaint = Complaint.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def complaint_params
-      params.require(:complaint).permit(:product, :complain_what_happened, :issue, :sub_product, :zip_code, :tags, :complaint_id, :timely, :consumer_consent_provided, :company_response, :submitted_via, :company, :date_received, :state, :consumer_disputed, :company_public_response, :sub_issue)
-    end
+  def set_complaint
+    @complaint = Complaint.find(params[:id])
+  end
+
+  def complaint_params
+    params.require(:complaint).permit(:complain_what_happened)
+  end
 end
