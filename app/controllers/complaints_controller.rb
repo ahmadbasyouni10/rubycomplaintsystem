eb7fb81db052ac7812c1da5a894bdd62021ffe24
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-08-18 09:43:30 -0400

    changes to controller

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,8 +1,9 @@
 class ComplaintsController < ApplicationController
-  before_action :set_complaint, only: %i[show edit update destroy]
+  before_action :set_complaint, only: [:show, :edit, :update, destroy]
 
   def index
-    @complaints = Complaint.all
+    # only show user created complaints
+    @complaints = Complaint.where(is_user_created: true)
   end
 
   def show
@@ -12,22 +13,24 @@ class ComplaintsController < ApplicationController
     @complaint = Complaint.new
   end
 
-  def edit
-  end
-
   def create
     @complaint = Complaint.new(complaint_params)
+    # set is_user_created to true
+    @complaint.is_user_created = true
 
     if @complaint.save
-      redirect_to @complaint, notice: 'Complaint was successfully created and categorized.'
+      redirect_to @complaint, notice: 'Complaint was successfully created.'
     else
       render :new
     end
   end
 
+  def edit
+  end
+
   def update
     if @complaint.update(complaint_params)
-      redirect_to @complaint, notice: 'Complaint was successfully updated and re-categorized.'
+      redirect_to @complaint, notice: 'Complaint was successfully updated.'
     else
       render :edit
     end
@@ -41,10 +44,10 @@ class ComplaintsController < ApplicationController
   private
 
   def set_complaint
-    @complaint = Complaint.find(params[:id])
+    @complaint = Complaint.where(is_user_created: true).find(params[:id])
   end
 
   def complaint_params
-    params.require(:complaint).permit(:complain_what_happened)
+    params.require(:complaint).permit(:product, :complain_what_happened, :issue, :sub_product, :zip_code, :tags, :timely, :consumer_consent_provided, :company_response, :submitted_via, :company, :date_received, :state, :consumer_disputed, :company_public_response, :sub_issue)
   end
 end
